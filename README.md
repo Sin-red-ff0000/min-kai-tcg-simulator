9c79c00c096dabf00aa045b183076b509d571436
Sin-red-ff0000 <2g4jcmwff2@privaterelay.appleid.com>
2026-08-27 01:02:19 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-みん会TCG Simulator v10.9 PWA版
+みん会TCG Simulator v10.9 r2 PWA版
 
 PWA一式です。index.html / manifest.webmanifest / sw.js / icons / frames を同じ階層構成で公開してください。
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'minkai-tcg-simulator-v10-9';
+const CACHE_NAME = 'minkai-tcg-simulator-v10-9-r2';
 const APP_SHELL = [
   './',
   './index.html',
