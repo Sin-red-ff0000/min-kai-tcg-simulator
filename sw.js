7bec9c6578141096dab299e40808fe95dc36e83f
Sin-red-ff0000 <2g4jcmwff2@privaterelay.appleid.com>
2026-08-28 02:36:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'minkai-tcg-simulator-v12-11-fx-cleanup';
+const CACHE_NAME = 'minkai-tcg-simulator-v12-12-material-pack';
 const APP_SHELL = [
   './',
   './index.html',
@@ -27,6 +27,14 @@ const APP_SHELL = [
   './frames/second-ancient-forest-temple.png',
   './frames/second-luxury-cyber.png',
   './frames/second-void-fracture.png',  './effects/frost-ice.png',  './effects/holographic-film-soft.png',
+  './effects/surface-diamond-cut.png',
+  './effects/surface-satin-foil.png',
+  './effects/surface-liquid-chrome.png',
+  './effects/surface-anodized-metal.png',
+  './effects/surface-glass-laminate.png',
+  './effects/surface-mother-pearl.png',
+  './effects/surface-carbon-iridescent.png',
+  './effects/surface-velvet-matte.png',
 ];
 
 self.addEventListener('install', event => {
