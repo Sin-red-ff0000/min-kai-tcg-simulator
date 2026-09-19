5d78c0a0434c6c4dacde638f4b363353ec791147
Sin-red-ff0000 <2g4jcmwff2@privaterelay.appleid.com>
2026-09-20 05:07:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='minkai-tcg-v28-3';
+const CACHE_NAME='minkai-tcg-v28-4';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
